7f50ebf51c78f331b5c0c6798f2c37e8760e608a
Ram Goel <rgoel766@gmail.com>
2023-03-29 23:17:48 +0530

    Initial Code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import './App.css';
-import CustomerHome from './components/CustomerHome';
-import ExpertHome from './components/ExpertHome';
-import Login from './components/Login';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import ErrorPage from './components/ErrorPage';
+import Home from './Home';
 
 
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element:<Login />,
-    errorElement:<ErrorPage />
-  },
-  {
-    path:'/customer',
-    element:<CustomerHome />,
-    errorElement:<ErrorPage />
-  },
-  {
-    path:'/expert',
-    element:<ExpertHome />,
-    errorElement:<ErrorPage />
-
-  },
-])
 function App() {
   return (
     <div className="App">
-      {/* <CustomerHome /> */}
-      <RouterProvider router={router} />
+      <Home />
     </div>
   );
 }

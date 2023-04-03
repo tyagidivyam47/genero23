d65b89582b27bf3c24cd1d992662d03a621bbafe
Ram Goel <rgoel766@gmail.com>
2023-04-03 14:59:02 +0530

    lint fix

--- src/Pages/Sponsors.jsx
+++ src/Pages/Sponsors.jsx
@@ -5,7 +5,6 @@ function Sponsors() {
         <div className='text-dark text-center'>
             <h4 className='my-3'>Our Sponsors</h4>
         <div class="slider">
-            <h4></h4>
             <div class="slide-track">
                 <div class="slide">
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />

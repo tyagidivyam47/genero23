b86f61cb847634b9214ad712b093a4170880bb6b
Ram Goel <rgoel766@gmail.com>
2023-04-25 18:41:15 +0530

    lint error fix

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import About from './Pages/About';
 import Events from './Pages/Events/Events';
 import FAQ from './Pages/FAQ/FAQ';
 import Home from './Pages/Home';
-import Footer from './Pages/Footer/Footer';
 import PreLoader from './Pages/PreLoader/PreLoader';
 import { useEffect, useState } from 'react';
 // import Team from './components/team/Team';
 import Registration from './Pages/Register/Registration';
 import TeamSection from './Pages/TeamSection';
-import Navbar from './Atoms/Navbar';
 // import TeamPage from './Pages/TeamPage/TeamPage';
 function App() {
   const { search } = useLocation();

d49e1a33abc9134f0ab67380e43274ce67736aec
Divyam Tyagi <tyagidivyam47@gmail.com>
2023-04-26 17:21:37 +0530

    Added scroll to team page

--- src/Pages/TeamPage/TeamPage.js
+++ src/Pages/TeamPage/TeamPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './teampage.css'
 import SubTeamData from '../../components/team/SubTeamData'
 import Team from '../../components/team/Team'
@@ -6,17 +6,22 @@ import Subteam from '../../components/team/Subteam'
 import Navbar from '../../Atoms/NavCopy'
 import Footer from '../Footer/Footer'
 
+
 export default function TeamPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  
   return (
     <div id='team'>
-      <Navbar id='team-navbar'/>
-      <Team/>
+      <Navbar id='team-navbar' />
+      <Team />
       <div className='subteam-unique'>
-        {SubTeamData.map((p) => 
-           <Subteam key={p.id} post={p}/>
+        {SubTeamData.map((p) =>
+          <Subteam key={p.id} post={p} />
         )}
       </div>
-        <Footer/>
+      <Footer />
     </div>
   )
 }

4c4c1a905270cc8e263b22eef2a0e0cccecafd52
Divyam Tyagi <tyagidivyam47@gmail.com>
2023-05-05 22:25:40 +0530

    Added event folder

---
+++ src/Pages/EventsList/EventsList.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import Navbar from '../../Atoms/NavCopy'
+
+import './EventsList.css';
+import EventCard from '../../Atoms/EventCard/EventCard';
+
+const EventsList = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+
+    return (
+
+        <div className='bd'>
+            <Navbar />
+
+            <div className="event-padded">
+
+            <div className='grid-container'>
+                <EventCard
+                    key={"list.id"}
+                    propStyle={{height:'200px'}}
+                    imgSrc="https://images.unsplash.com/photo-1586182987320-4f376d39d787?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    name="E-Gaming"
+                    redirectLink="/events/egaming"
+                />
+
+                <EventCard
+                    key={"list.id"}
+                    propStyle={{height:'200px'}}
+                    imgSrc="https://images.unsplash.com/photo-1603587979462-2591fd6f5144?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    name="Informal"
+                    redirectLink="/events/informal"
+                />
+
+                <EventCard
+                    key={"list.id"}
+                    propStyle={{height:'200px'}}
+                    imgSrc="https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                    name="Photography"
+                    redirectLink="/events/photography"
+                />  
+
+                <EventCard
+                    key={"list.id"}
+                    propStyle={{height:'200px'}}
+                    imgSrc="https://images.unsplash.com/photo-1535905557558-afc4877a26fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                    name="Literary"
+                    redirectLink="/events/literary"
+                />
+
+                <EventCard
+                    key={"list.id"}
+                    propStyle={{height:'200px'}}
+                    imgSrc="https://images.unsplash.com/photo-1546778316-dfda79f1c84e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    name="Cultural"
+                    redirectLink="/events/cultural"
+                />
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default EventsList
---
+++ src/Pages/EventsList/Photography/PhotographyList.jsx
@@ -0,0 +1,43 @@
+// import classes from "./Technical.module.css";
+// import TechnicalLists from "../TechnicalDetail/TechnicalLists";
+import PhotographyData from "../../../EventsData/PhotographyData";
+import { useEffect } from "react";
+import EventCard from "../../../Atoms/EventCard1/EventCard";
+
+import "./PhotographyList.css"
+import Navbar from "../../../Atoms/NavCopy";
+
+const PhotographyList = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
+
+    // console.log(CulturalData);
+
+    return (
+        <div className="bd">
+            <Navbar />
+            <div className="event-padded">
+
+            <h1 style={{ textAlign: "center" }}>Photography</h1>
+            <div className="grid-container">
+                {PhotographyData.map((list,index) => {
+                    // {console.log(list.heading)}
+                    return (
+                        <EventCard
+                            key={list.id}
+                            serial={index+1}
+                            imgSrc={list.imgSrc}
+                            propStyle={{height:'200px'}}
+                            name={list.heading}
+                            redirectLink={list.redirectLink}
+                        />
+                    );
+                })}
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default PhotographyList;

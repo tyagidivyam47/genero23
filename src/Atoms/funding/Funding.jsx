9777e3d8751fb90ccf7edc6bec3cf1ed55a4977b
amarjyothi98 <amarjyothi526@gmail.com>
2023-05-09 21:58:53 +0530

    added sponsorship skeleton for pratyush

---
+++ src/Atoms/funding/Funding.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './funding.css'
+
+export default function Funding() {
+  return (
+    <div>
+      <div className="funding">
+        anything
+      </div>
+
+    </div>
+  )
+}

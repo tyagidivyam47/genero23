4d66f2a3ae8ea542ece912af0cd01a3e457ae445
Kuroo7 <atif40666@gmail.com>
2023-05-11 13:04:14 +0530

    Updated Navbar with sponsor link

--- src/Pages/Sponsors/Sponsorship.jsx
+++ src/Pages/Sponsors/Sponsorship.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Navbar from '../../Atoms/navbar/Navbar'
+import Navbar from '../../Atoms/navbar/NavCopy'
 import Funding from '../../Atoms/funding/Funding'
 import Footer from '../../Atoms/footer/Footer'
 
 export default function Sponsorship() {
   return (
     <>
-    <Navbar/>
-    
-    <Funding/>
+      <Navbar />
 
-    <Footer/>
+      <Funding />
+
+      <Footer />
     </>
   )
 }
